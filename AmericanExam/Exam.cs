1f81597d36f0728dc09a8bc7aba2ad675f8107cc
rachel <rachelpolw@gmail.com>
2023-05-08 15:16:37 +0300

    Add all files

---
+++ AmericanExam/Exam.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmericanExam
+{
+    public class Exam
+    {
+        public string DisplayName { get; set; }
+
+        private List<Question> _questions = new List<Question>();
+
+        public List<Question> Questions
+        {
+            get { return _questions; }
+            set { _questions = value; }
+        }
+    }
+}
---
+++ AmericanExam/Question.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmericanExam
+{
+    public class Question
+    {
+        public bool Id { get; set; }
+        public string Caption { get; set; }
+
+        private List<string> _answers = new List<string>();
+
+        public List<string> Answers
+        {
+            get { return _answers; }
+            set { _answers  = value; }
+        }
+
+        public int CorrectAnswer { get; set; }
+      
+    }
+}
